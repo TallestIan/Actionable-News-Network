29b311d02c8fa612a5f09f8e17b6f82ac15a6057
Ian Geiman <ian.geiman@me.com>
2020-04-23 13:18:55 -0400

    Initial Commit
    Things work yo

---
+++ background.js
@@ -0,0 +1,45 @@
+// Called when the user clicks on the browser action  ----------------------------------------------------
+chrome.browserAction.onClicked.addListener(function(tab) {
+    // Send a message to the active tab
+    chrome.tabs.query({active: true, currentWindow: true}, function(tabs) {
+        var activeTab = tabs[0];
+        chrome.tabs.sendMessage(activeTab.id, {"message": "clicked_browser_action"});
+    });
+});
+
+
+//Firebase Configuration ----------------------------------------------------
+const fbConfig = {
+    apiKey: "AIzaSyDuWnztNzNz8eiqLYnezr9KamIaLt95tfs",
+    authDomain: "actionnn-2ac77.firebaseapp.com",
+    databaseURL: "https://actionnn-2ac77.firebaseio.com",
+    projectId: "actionnn-2ac77",
+    storageBucket: "actionnn-2ac77.appspot.com",
+    messagingSenderId: "765781008857",
+    appId: "1:765781008857:web:518af75671f5a5dcc9b9c8"
+};
+
+firebase.initializeApp(fbConfig);
+const db = firebase.firestore();
+
+
+//Grabbing General Data from Firestore ----------------------------------------------------
+
+
+//Grabbing Specific Data from Firestore ----------------------------------------------------
+let fillData = '';
+
+db.collection("topics").doc("29IhYhNYI7ZUQhD8TjDG").get().then((doc) => {
+    console.log(doc.data());
+    fillData = doc.data();
+
+});
+
+chrome.runtime.onMessage.addListener(
+    function(request, sender, sendResponse) {
+        console.log(sender.tab ?
+            "from a content script:" + sender.tab.url :
+            "from the extension");
+        if (request.topic === "Coronavirus")
+            sendResponse(fillData);
+    });
---
+++ js/content.js
@@ -0,0 +1,72 @@
+// Adding Stylesheet to Popup ----------------------------------------------------
+
+let link = document.createElement("link");
+link.href = chrome.extension.getURL("css/popup.css");
+link.type = "text/css";
+link.rel = "stylesheet";
+document.getElementsByTagName("head")[0].appendChild(link);
+
+
+//Page Topic Identification ----------------------------------------------------
+let topics = [];
+$(".tags-list__tags a").each(function() {topics.push($(this).text())});
+// console.log(topics);
+
+
+//Database Topic Loading ----------------------------------------------------
+chrome.runtime.sendMessage({topics}, function(response) {
+    // console.log(response);
+});
+
+
+//Grabbing and Adding Firestore data into HTML  ----------------------------------------------------
+for (i = 0; i < topics.length; i++) {
+    if(topics[i].match('Coronavirus')) {
+
+        chrome.runtime.sendMessage({topic: "Coronavirus"}, function(response) {
+            // console.log(response);
+            $.get(chrome.extension.getURL('/pageFrame.html'), function(data) {
+                $(data).appendTo('body');
+                $("#AnnEvent").text(response.event);
+                $("#AnnImage").attr("src", response.imageLink);
+                $("#AnnBlurb").text(response.blurb);
+                $("#AnnBlurbLink").attr("href", response.blurbLink);
+                $("#AnnInform").text(response.inform);
+                $("#AnnInformLink").attr("href", response.informLink);
+                $("#AnnCreate").text(response.create);
+                $("#AnnCreateLink").attr("href", response.createLink);
+                $("#AnnDonate").text(response.donate);
+                $("#AnnDonateLink").attr("href", response.donateLink);
+            });
+        });
+        break;
+
+    }
+}
+
+
+
+
+//Old Shit  --------------------------------------------------------------------------------------------------------
+// if($(".tags-list__tags a").text().match('Coronavirus')) {
+//
+//     chrome.runtime.sendMessage({topic: "Coronavirus"}, function(response) {
+//         // console.log(response);
+//         $.get(chrome.extension.getURL('/pageFrame.html'), function(data) {
+//             $(data).appendTo('body');
+//             $("#AnnEvent").text(response.event);
+//             $("#AnnImage").attr("src", response.imageLink);
+//             $("#AnnBlurb").text(response.blurb);
+//             $("#AnnBlurbLink").attr("href", response.blurbLink);
+//             $("#AnnInform").text(response.inform);
+//             $("#AnnInformLink").attr("href", response.informLink);
+//             $("#AnnCreate").text(response.create);
+//             $("#AnnCreateLink").attr("href", response.createLink);
+//             $("#AnnDonate").text(response.donate);
+//             $("#AnnDonateLink").attr("href", response.donateLink);
+//         });
+//     });
+//
+// } else {
+//     console.log("Not a supported topic")
+// }
